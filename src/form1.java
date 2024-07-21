084c12db336605715bd0d8b972ce3dc9d52b68b7
David Muela <stalinsangucho87@gmail.com>
2024-07-20 20:04:03 -0500

    Modificacion Visual del Login

--- src/form1.java
+++ src/form1.java
@@ -5,11 +5,18 @@ import java.awt.event.ActionListener;
 public class form1 {
     public JPanel panel1;
     private JComboBox comboBox1;
-    private JLabel label;
     private JPanel panelEstudiante;
     private JTextField textField1;
     private JPasswordField passwordField1;
-    private JButton button1;
+    private JButton INGRESARButton;
+    private JPasswordField passwordField2;
+    private JButton INGRESARButton2;
+    private JPanel panelAdministrativo;
+    private JTextField textField2;
+    private JPasswordField passwordField3;
+    private JButton INGRESARButton1;
+    private JPanel panelProfesor;
+    private JTextField textField3;
 
     public form1() {
         comboBox1.addActionListener(new ActionListener() {
@@ -19,12 +26,18 @@ public class form1 {
                 switch (eleccion) {
                     case "Estudiante":
                         panelEstudiante.setVisible(true);
+                        panelProfesor.setVisible(false);
+                        panelAdministrativo.setVisible(false);
                         break;
                     case "Profesor":
+                        panelProfesor.setVisible(true);
                         panelEstudiante.setVisible(false);
+                        panelAdministrativo.setVisible(false);
                         break;
                     case "Administrador":
-                        label.setText("Administrador");
+                        panelAdministrativo.setVisible(true);
+                        panelProfesor.setVisible(false);
+                        panelEstudiante.setVisible(false);
                         break;
                     default:
                         System.out.printf("ERROR");
