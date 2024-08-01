cc21e0ab352fe4749ac73dfeebe2f40410cc44bb
David Muela <stalinsangucho87@gmail.com>
2024-07-31 22:05:15 -0500

    Documentacion

--- src/Administrador/PerfilAdmin.java
+++ src/Administrador/PerfilAdmin.java
@@ -4,7 +4,10 @@ import Login.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.Normalizer;
-
+/**
+ * La clase {@code PerfilAdmin} representa el perfil del administrador
+ * y proporciona funcionalidades para agregar, visualizar, borrar y cerrar sesión.
+ */
 public class PerfilAdmin {
     public JPanel paneladmin;
     private JButton CERRARSESIONButton;
@@ -12,15 +15,27 @@ public class PerfilAdmin {
     private JButton VISUALIZARButton;
     private JButton BORRARButton;
 
+    /**
+     * Constructor de la clase {@code PerfilAdmin}
+     * Configura los botones y sus respectivos eventos
+     */
+
     public PerfilAdmin() {
+        /**
+         * @param botonos Permite crear ActionListener de los diferentes acciones como ADMINISTRADOR
+         */
+
         AGREGARButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // Crear una nueva ventana para el módulo de agregar administrador
                 JFrame frame = new JFrame();
                 frame.setContentPane(new AgregarAdmin().agregarAdmin);
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 frame.pack();
                 frame.setVisible(true);
+
+                // Cierra la ventana actual del perfil de administrador
                 ((JFrame) SwingUtilities.getWindowAncestor(AGREGARButton)).dispose();
 
             }
@@ -28,11 +43,14 @@ public class PerfilAdmin {
         CERRARSESIONButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // Crear una nueva ventana para la pantalla de inicio de sesión
                 JFrame frame = new JFrame();
                 frame.setContentPane(new form1().panel1);
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 frame.pack();
                 frame.setVisible(true);
+
+                // Cierra la ventana actual del perfil de administrador
                 ((JFrame) SwingUtilities.getWindowAncestor(CERRARSESIONButton)).dispose();
 
             }
@@ -40,11 +58,15 @@ public class PerfilAdmin {
         BORRARButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
+                // Crear una nueva ventana para el módulo de borrar administrador
                 JFrame frame = new JFrame();
                 frame.setContentPane(new BorrarAdmin().AdminBorrar);
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 frame.pack();
                 frame.setVisible(true);
+
+                // Cierra la ventana actual del perfil de administrador
                 ((JFrame) SwingUtilities.getWindowAncestor(BORRARButton)).dispose();
 
             }
@@ -52,12 +74,16 @@ public class PerfilAdmin {
         VISUALIZARButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
+                // Crear una nueva ventana para el módulo de visualizar administradores
                 JFrame frame = new JFrame("MostrarAdmin");
                 frame.setContentPane(new MostrarAdmin().mostrarAdmin);
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 frame.pack();
                 frame.setSize(800, 600);
                 frame.setVisible(true);
+
+                // Cierra la ventana actual del perfil de administrador
                 ((JFrame) SwingUtilities.getWindowAncestor(VISUALIZARButton)).dispose();
 
 
