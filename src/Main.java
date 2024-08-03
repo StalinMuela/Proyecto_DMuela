f6ccb20a257cc2a704299af2d027a239f80f1d02
David Muela <stalinsangucho87@gmail.com>
2024-08-03 16:01:31 -0500

    DOCUMENTACION

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main {
         //Permite crear un panel conocido como INICIO DE SESION
 
         JFrame frame = new JFrame("GESTION AULAS ESFOT");
-        frame.setContentPane(new form1().panel1);
+        frame.setContentPane(new PerfilEstudiante().panelEstudiantes);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.setSize(800, 600);
